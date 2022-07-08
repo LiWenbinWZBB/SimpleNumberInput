36e096cc9512e22391529a81ee995c89a64de35d
WBLi <8936282+LiWenbinWZBB@users.noreply.github.com>
2022-07-08 13:40:38 +0800

    Release V0.1.0

--- SimpleNumberInput/SimpleNumberInput/Properties/AssemblyInfo.cs
+++ SimpleNumberInput/SimpleNumberInput/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SimpleNumberInput")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple number input WPF control")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleNumberInput")]
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
